c4c09e01cd6c3f187ad4be9286d4f3e881a65eb6
GustavoArielRos <GustavoAriRoss@gmail.com>
2024-01-13 04:58:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react' 
 import './App.css';
 
 function App() {
+
+  const [nome,setNome] = useState();
+  const [id,setId] = useState(0);
+
+  const armazenar = (chave,valor) =>{
+    localStorage.setItem(chave,valor)
+    setId(id + 1);
+  }
+
+  const consultar=(chave) =>{
+    alert(localStorage.getItem(chave))
+  }
+
+  const apagar =(chave) => {
+    localStorage.removeItem(chave)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Digite um nome</label><br></br>
+      <input type="text" value={nome} onChange={(e) => setNome(e.target.value)}></input><br></br>
+      <button onClick={() => armazenar(id, nome)}>Gravar Nome</button>
+      <button onClick={() => consultar('ls nome')}>Ver nome</button>
+      <button onClick={() => apagar(0)}>Apagar nome</button>
     </div>
   );
 }
